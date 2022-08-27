2764e65d8dd7236cbaf106bc829c91d1b0e536c4
Etomidate <68708079+Etomidate@users.noreply.github.com>
2022-08-27 13:58:54 -0700

    axl-first-consoleapp-bday-list
    Axl's first console app. His bday wish list.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Axl bday list
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is Axl's Birthday List!");
+
+        string firstGift = "Pokemon ScoreBunny plushie";
+        string secondGift = "Bablade achillies and ultraalchillies with launcher";
+        string thirdGift = "Pokemon watch with games on it";
+        string fourthGift = "Lego minecraft fox set";
+        string fifthGift = "bablade arena";
+
+
+        Console.WriteLine("1. " + firstGift + "\n2. " + secondGift + "\n3. " + thirdGift + "\n4. " + fourthGift + "\n5. " + fifthGift);
+    }
+}
